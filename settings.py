0fe179728ebf861273c9bd4be7cd24bd0ccd1cfa
giacomo-b <giacomo.bonaccorsi@polimi.it>
2020-11-09 14:39:29 +0100

    Updated settings.py

--- settings.py
+++ settings.py
@@ -2,16 +2,16 @@ topics = []
 
 topics.append({
     "name":             "first_topic",
-    "subreddits":       [ {"name":        "BetterEveryLoop", "sort": "top", "time": "month", "limit": 10},
-                          {"name":      "interestingasfuck", "sort": "top", "time": "month", "limit": 10} ],
+    "subreddits":       [ {"name":        "BetterEveryLoop", "sort": "top", "time": "month", "limit": 50},
+                          {"name":      "interestingasfuck", "sort": "top", "time": "month", "limit": 50} ],
     "MAX_CLIP_TIME":    10 * 60.0,  # seconds
-    "MAX_SUBCLIP_TIME": 10.0,       # seconds
+    "MAX_SUBCLIP_TIME": 20.0,       # seconds
 })
 
 topics.append({
     "name":             "second_topic",
-    "subreddits":       [ {"name":      "AnimalsBeingJerks", "sort": "top", "time": "month", "limit": 10},
-                          {"name":      "AnimalsBeingDerps", "sort": "top", "time": "month", "limit": 10} ],
+    "subreddits":       [ {"name":      "AnimalsBeingJerks", "sort": "top", "time": "month", "limit": 50},
+                          {"name":      "AnimalsBeingDerps", "sort": "top", "time": "month", "limit": 50} ],
     "MAX_CLIP_TIME":    10 * 60.0,  # seconds
-    "MAX_SUBCLIP_TIME": 10.0,       # seconds
+    "MAX_SUBCLIP_TIME": 20.0,       # seconds
 })
